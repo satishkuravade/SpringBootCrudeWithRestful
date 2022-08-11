4b622d8fd7e7a2d788d6b487d9a89198e845e933
satish kuravade <kuravadesatish@gmail.com>
2022-08-12 01:05:39 +0530

    project added to central repository

---
+++ src/main/java/com/example/SpringBootcrudeWithRestful/SpringBootcrudeWithRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcrudeWithRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcrudeWithRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcrudeWithRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootcrudeWithRestful/SpringBootcrudeWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcrudeWithRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcrudeWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
